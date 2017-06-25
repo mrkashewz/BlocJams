81d9670749cf97ce6aee8a867a301f0ea1989805
Ishar Keshu <ishkeshu@gmail.com>
2017-06-24 21:58:40 -0400

    checkpoint 26 : completed

--- scripts/album.js
+++ scripts/album.js
@@ -6,11 +6,11 @@ var albumPicasso = {
     year: '1881',
     albumArtUrl: 'assets/images/album_covers/01.png',
     songs: [
-        { title: 'Blue', duration: '4:26' },
-        { title: 'Green', duration: '3:14' },
-        { title: 'Red', duration: '5:01' },
-        { title: 'Pink', duration: '3:21'},
-        { title: 'Magenta', duration: '2:15'}
+        { name: 'Blue', length: '4:26' },
+        { name: 'Green', length: '3:14' },
+        { name: 'Red', length: '5:01' },
+        { name: 'Pink', length: '3:21'},
+        { name: 'Magenta', length: '2:15'}
     ]
 };
 
@@ -22,11 +22,11 @@ var albumMarconi = {
     year: '1909',
     albumArtUrl: 'assets/images/album_covers/20.png',
     songs: [
-        { title: 'Hello, Operator?', duration: '1:01' },
-        { title: 'Ring, ring, ring', duration: '5:01' },
-        { title: 'Fits in your pocket', duration: '3:21'},
-        { title: 'Can you hear me now?', duration: '3:14' },
-        { title: 'Wrong phone number', duration: '2:15'}
+        { name: 'Hello, Operator?', length: '1:01' },
+        { name: 'Ring, ring, ring', length: '5:01' },
+        { name: 'Fits in your pocket', length: '3:21'},
+        { name: 'Can you hear me now?', length: '3:14' },
+        { name: 'Wrong phone number', length: '2:15'}
     ]
 };
 
@@ -38,18 +38,18 @@ var albumNirvana = {
     albumArtUrl: 'assets/images/album_covers/22.png',
     genre: 'grunge',
     songs: [
-        { title: 'Smells Like Teen Spirit', duration: '5:01' },
-        { title: 'In Bloom', duration: '4:14' },
-        { title: 'Come as You Are', duration: '3:03'},
-        { title: 'Breed', duration: '3:14' },
-        { title: 'Lithium', duration: '4:17' },
-        { title: 'Polly', duration: '2:57' },
-        { title: 'Territorial Pissings', duration: '2:22'},
-        { title: 'Drain You', duration: '3:43' },
-        { title: 'Lounge Act', duration: '2:36' },
-        { title: 'Stay Away', duration: '3:32' },
-        { title: 'On a Plain', duration: '3:16'},
-        { title: 'Something in the Way', duration: '3:46' },
+        { name: 'Smells Like Teen Spirit', length: '5:01' },
+        { name: 'In Bloom', length: '4:14' },
+        { name: 'Come as You Are', length: '3:03'},
+        { name: 'Breed', length: '3:14' },
+        { name: 'Lithium', length: '4:17' },
+        { name: 'Polly', length: '2:57' },
+        { name: 'Territorial Pissings', length: '2:22'},
+        { name: 'Drain You', length: '3:43' },
+        { name: 'Lounge Act', length: '2:36' },
+        { name: 'Stay Away', length: '3:32' },
+        { name: 'On a Plain', length: '3:16'},
+        { name: 'Something in the Way', length: '3:46' },
     ]
 };
 
@@ -91,6 +91,10 @@ var songRows = document.getElementsByClassName('album-view-song-item');
 
 // Album button templates
  var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
+ var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
+
+ var currentlyPlayingSong = null;
+
 
  window.onload = function() {
      setCurrentAlbum(albumPicasso);
@@ -98,7 +102,11 @@ var songRows = document.getElementsByClassName('album-view-song-item');
      songListContainer.addEventListener('mouseover', function(event) {
     // #1
     if (event.target.parentElement.className === 'album-view-song-item') {
-         event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
+                  var songItem = getSongItem(event.target);
+
+            if (songItem.getAttribute('data-song-number') !== currentlyPlayingSong) {
+                songItem.innerHTML = playButtonTemplate;
+            }
     }
 });
 for (var i = 0; i < songRows.length; i++) {
@@ -107,6 +115,9 @@ for (var i = 0; i < songRows.length; i++) {
          this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
 
      });
+     songRows[i].addEventListener('click', function(event) {
+     clickHandler(event.target);
+    });
  }
 
      var albums = [albumPicasso, albumMarconi, albumNirvana];
@@ -118,4 +129,48 @@ for (var i = 0; i < songRows.length; i++) {
          index= 0;
        }
      });
+     var findParentByClassName = function(element, targetClass) {
+         if (element) {
+             var currentParent = element.parentElement;
+             while (currentParent.className !== targetClass && currentParent.className !== null) {
+                 currentParent = currentParent.parentElement;
+             }
+             return currentParent;
+         }
+     };
+
+    var getSongItem = function(element) {
+      switch (element.className) {
+          case 'album-song-button':
+          case 'ion-play':
+          case 'ion-pause':
+              return findParentByClassName(element, 'song-item-number');
+          case 'album-view-song-item':
+              return element.querySelector('.song-item-number');
+          case 'song-item-title':
+          case 'song-item-duration':
+              return findParentByClassName(element, 'album-view-song-item').querySelector('.song-item-number');
+          case 'song-item-number':
+              return element;
+          default:
+              return;
+    }
+};
+
+var clickHandler = function(targetElement){
+  var songItem = getSongItem(targetElement);
+
+  if (currentlyPlayingSong === null) {
+    songItem.innerHTML = pauseButtonTemplate;
+    currentlyPlayingSong = songItem.getAttribute('data-song-number');
+  } else if (currentlyPlayingSong === songItem.getAttribute('data-song-number')) {
+  songItem.innerHTML = playButtonTemplate;
+  currentlyPlayingSong = null;
+} else if (currentlyPlayingSong !== songItem.getAttribute('data-song-number')) {
+     var currentlyPlayingSongElement = document.querySelector('[data-song-number="' + currentlyPlayingSong + '"]');
+     currentlyPlayingSongElement.innerHTML = currentlyPlayingSongElement.getAttribute('data-song-number');
+     songItem.innerHTML = pauseButtonTemplate;
+     currentlyPlayingSong = songItem.getAttribute('data-song-number');
+ }
+}
  };
