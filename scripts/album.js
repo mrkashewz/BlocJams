4da72fbef5a67d15cda57d9a42da02c53938b43c
Ishar Keshu <ishkeshu@gmail.com>
2017-06-26 14:19:47 -0400

    assignment 26 help

--- scripts/album.js
+++ scripts/album.js
@@ -138,12 +138,27 @@ for (var i = 0; i < songRows.length; i++) {
      var findParentByClassName = function(element, targetClass) {
          if (element) {
              var currentParent = element.parentElement;
+
+             var hasNoParent = !document.getElementById("targetClass").className;
+
+             if(targetClass === null) {
+               console.log("No parent found.")
+             }
+
+             var hasNoParentClassName =
+              !document.getElementbyId("targetClass").className;
+
+              if(currentParent == null){
+                console.log("No parent found with that class name.")
+              }
+
+
+
              while (currentParent.className !== targetClass && currentParent.className !== null) {
                  currentParent = currentParent.parentElement;
              }
              return currentParent;
          }
-     };
 
     var getSongItem = function(element) {
       switch (element.className) {
