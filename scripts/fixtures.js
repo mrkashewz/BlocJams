433144472d5c12e4a1304a4076696b6f61f79185
Ishar Keshu <ishkeshu@gmail.com>
2017-07-03 14:07:11 -0400

    checkpoint 31 completed

---
+++ scripts/fixtures.js
@@ -0,0 +1,54 @@
+// Example Album
+var albumPicasso = {
+    name: 'The Colors',
+    artist: 'Pablo Picasso',
+    label: 'Cubism',
+    year: '1881',
+    albumArtUrl: 'assets/images/album_covers/01.png',
+    songs: [
+        { name: 'Blue', length: '4:26' },
+        { name: 'Green', length: '3:14' },
+        { name: 'Red', length: '5:01' },
+        { name: 'Pink', length: '3:21'},
+        { name: 'Magenta', length: '2:15'}
+    ]
+};
+
+// Another Example Album
+var albumMarconi = {
+    name: 'The Telephone',
+    artist: 'Guglielmo Marconi',
+    label: 'EM',
+    year: '1909',
+    albumArtUrl: 'assets/images/album_covers/20.png',
+    songs: [
+        { name: 'Hello, Operator?', length: '1:01' },
+        { name: 'Ring, ring, ring', length: '5:01' },
+        { name: 'Fits in your pocket', length: '3:21'},
+        { name: 'Can you hear me now?', length: '3:14' },
+        { name: 'Wrong phone number', length: '2:15'}
+    ]
+};
+
+var albumNirvana = {
+    name: 'Nevermind',
+    artist: 'Nirvana',
+    label: 'DCG',
+    year: '1991',
+    albumArtUrl: 'assets/images/album_covers/22.png',
+    genre: 'grunge',
+    songs: [
+        { name: 'Smells Like Teen Spirit', length: '5:01' },
+        { name: 'In Bloom', length: '4:14' },
+        { name: 'Come as You Are', length: '3:03'},
+        { name: 'Breed', length: '3:14' },
+        { name: 'Lithium', length: '4:17' },
+        { name: 'Polly', length: '2:57' },
+        { name: 'Territorial Pissings', length: '2:22'},
+        { name: 'Drain You', length: '3:43' },
+        { name: 'Lounge Act', length: '2:36' },
+        { name: 'Stay Away', length: '3:32' },
+        { name: 'On a Plain', length: '3:16'},
+        { name: 'Something in the Way', length: '3:46' },
+    ]
+};
